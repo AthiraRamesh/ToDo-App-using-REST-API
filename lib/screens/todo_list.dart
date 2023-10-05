a7071906a7baf395a26f9b422a461bd65a66e56b
AthiraRamesh <athirarameshm@gmail.com>
2023-10-05 15:15:01 +0530

    Adding HTTP

---
+++ lib/screens/todo_list.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:todo_api/screens/add_page.dart';
+
+class TodoListPage extends StatefulWidget {
+  const TodoListPage({super.key});
+
+  @override
+  State<TodoListPage> createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("To Do List")),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: navigateToAddPage, label: Text("Add Todo")),
+    );
+  }
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(
+      builder: (context) => AddTodoPage(),
+    );
+    Navigator.push(context, route);
+  }
+}
